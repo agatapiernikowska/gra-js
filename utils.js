699d03e1a56d184bbc417152e8c415503a4b8a39
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-05-05 19:38:57 +0200

    fruits on board

--- utils.js
+++ utils.js
@@ -1,28 +1,63 @@
-function createTable (width, height) {
-  var table = document.createElement('table');
-  var tbody = document.createElement('tbody');
-  var tr, td, x, y;
-
-  for (y = 0; y < height; y += 1) {
-    tr = document.createElement('tr');
-    for (x = 0; x < width ; x += 1) {
-      td = document.createElement('td');
-      tr.appendChild(td);
-    }
-    tbody.appendChild(tr);
+function doTimes(howManyCalls, whatToDo) {
+  for (var i = 0; i < howManyCalls; i += 1) {
+    whatToDo(i);
   }
+}
 
-  table.appendChild(tbody);
+function makeNode(name) {
+  return document.createElement(name);
+}
+
+function makeCell(x, y) {
+  var cell = makeNode('td');
+  cell.innerText = x + ', ' + y;
+  return cell;
+}
+
+function makeRow(y, size) {
+  var tr = makeNode('tr');
+  doTimes(size, function (x) {
+    tr.appendChild(makeCell(x, y));
+  });
+  return tr;
+}
+
+function makeTableBody(width, height) {
+  var tbody = makeNode('tbody');
+  doTimes(height, function (rowIndex) {
+    var tr = makeRow(rowIndex, width);
+    tbody.appendChild(tr);
+  });
+  return tbody;
+}
+
+function createTable(width, height) {
+  var table = makeNode('table');
+  table.appendChild(makeTableBody(width, height));
   return table;
 }
 
-function getCell (table,x,y) {
-  var row = table.querySelector('tr:nth-child(' + (y+1) + ')');
-  return row.querySelector('td:nth-child(' + (x+1) + ')');
+function getCell(table, x, y) {
+  var row = table.querySelector('tr:nth-child(' + (y + 1) +  ')');
+  return row.querySelector('td:nth-child(' + (x + 1) + ')');
 }
 
-function colorize (node,color) {
+function colorize(node, color) {
   node.style.backgroundColor = color
 }
 
+function empty(node) {
+  // node.innerHTML = ''
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+}
+
+function inRange(number, begin, end) {
+  return number >= begin && number <= end
+}
+
+function noop (x) {
+  return x
+}
 
